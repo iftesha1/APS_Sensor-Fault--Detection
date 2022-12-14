e912639e5caa8e84f35da371547eed5d93e3b367
iftesha1 <ifteshanajnin786@gmail.com>
2022-12-14 22:12:57 +0530

    updated files

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+import os,sys
+from sensor.logger import logging
 
+def test_exception():
+    try:
+        logging.info("the test for logging by dividing")
+        x=1/0
+    except Exception as e:
+        raise SensorException(e,sys)
 
 if __name__=='__main__':
-    mongo_client=MongoDBClient()
-    print("collection names:",mongo_client.database.list_collection_names())
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
+
+
+    # mongo_client=MongoDBClient()
+    # print("collection names:",mongo_client.database.list_collection_names())
---
+++ sensor/constant/application.py
@@ -0,0 +1,3 @@
+#on which port we will be using fast-api to predict
+APP_HOST = "0.0.0.0"  
+APP_PORT = 8080
